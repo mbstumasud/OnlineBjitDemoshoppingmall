242d885c8a4ecefce78c935aa0e3ef9fbba77ab1
m.rana <m.rana@bjitgroup.com>
2018-02-06 18:17:47 +0600

    initial project with spring  boot starter web

---
+++ src/main/java/com/bjitgroup/OnlineBjitDemoShoppingMall/OnlineBjitDemoShoppingMallApplication.java
@@ -0,0 +1,12 @@
+package com.bjitgroup.OnlineBjitDemoShoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBjitDemoShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBjitDemoShoppingMallApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bjitgroup/OnlineBjitDemoShoppingMall/OnlineBjitDemoShoppingMallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bjitgroup.OnlineBjitDemoShoppingMall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineBjitDemoShoppingMallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
